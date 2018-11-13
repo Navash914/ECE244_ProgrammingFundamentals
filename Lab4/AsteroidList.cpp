4d4e3cd2a720f22e87c9a3a71c3e6f781aa876d4
Navash914 <35701995+Navash914@users.noreply.github.com>
2018-11-13 15:14:39 -0500

    Update AsteroidList.cpp

--- Lab4/AsteroidList.cpp
+++ Lab4/AsteroidList.cpp
@@ -2,151 +2,128 @@
 #include "AsteroidList.hpp"
 
 AsteroidListItem::AsteroidListItem() {
-	this->next = nullptr;
-	this->data = nullptr;
+    this->next = nullptr;
+    this->data = nullptr;
 }
 
 AsteroidListItem::AsteroidListItem(Asteroid a) {
-	this->next = nullptr;
-	this->data = new Asteroid(a);
+    this->next = nullptr;
+    this->data = new Asteroid(a);
 }
 
 AsteroidListItem::~AsteroidListItem() {
-	delete data;
+    delete data;
 }
 
 // head.next is automatically assigned to nullptr
 // via the AsteroidListItem default constructor
 AsteroidList::AsteroidList() = default;
 
-AsteroidList::AsteroidList(const AsteroidList& src)
-{
-	insertAfter(&head, src);
+AsteroidList::AsteroidList(const AsteroidList& src) {
+    insertAfter(&head, src);
 }
 
 AsteroidList::~AsteroidList() {
-	clear();
+    clear();
 }
 
-// Adds new list item at the front of the list
 void AsteroidList::pushFront(Asteroid e) {
-	AsteroidListItem* newItem = new AsteroidListItem(e);
-	newItem->setNext(head.getNext());
-	head.setNext(newItem);
+    insertAfter(&head, e);
 }
 
-// Returns data from the first list item
 Asteroid& AsteroidList::front() {
-	if (!isEmpty())
-		return head.getNext()->getData();
-	else
-		return *(Asteroid*)nullptr;
+    if (!isEmpty())
+        return head.getNext()->getData();
+    else
+        return *(Asteroid*)nullptr;
 }
 
 const Asteroid& AsteroidList::front() const {
-	if (!isEmpty())
-		return head.getNext()->getData();
-	else
-		return *(const Asteroid*)nullptr;
+    if (!isEmpty())
+        return head.getNext()->getData();
+    else
+        return *(const Asteroid*)nullptr;
 }
 
 bool AsteroidList::isEmpty() const {
-	return head.getNext() == nullptr;
+    return !head.hasNext();
 }
 
-// Calculates and returns size of list
 int AsteroidList::size() const {
-	int sz = 0;
-	const AsteroidListItem* iterator = &head;
-	while (iterator->hasNext()) {
-		sz++;
-		iterator = iterator->getNext();
-	}
-	return sz;
+    int sz = 0;
+    for (const AsteroidListItem* it = begin(); it != end(); 
+            it = it->getNext(), sz++);
+    return sz;
 }
 
-// Returns pointer to element before the first (ie head)
 AsteroidListItem* AsteroidList::beforeBegin() {
-	return &head;
+    return &head;
 }
 
 const AsteroidListItem* AsteroidList::beforeBegin() const {
-	return &head;
+    return &head;
 }
 
-// Returns pointer to first element of the list
 AsteroidListItem* AsteroidList::begin() {
-	return head.getNext();
+    return head.getNext();
 }
 
 const AsteroidListItem* AsteroidList::begin() const {
-	return head.getNext();
+    return head.getNext();
 }
 
-// Returns last element of the list
 AsteroidListItem* AsteroidList::beforeEnd() {
-	AsteroidListItem* iterator = &head;
-	while (iterator->hasNext())
-		iterator = iterator->getNext();
-	return iterator;
+    AsteroidListItem* it = beforeBegin();
+    while (it->hasNext())
+            it = it->getNext();
+    return it;
 }
 
 const AsteroidListItem* AsteroidList::beforeEnd() const {
-	const AsteroidListItem* iterator = &head;
-	while (iterator->hasNext())
-		iterator = iterator->getNext();
-	return iterator;
+    const AsteroidListItem* it = beforeBegin();
+    while (it->hasNext())
+            it = it->getNext();
+    return it;
 }
 
-// Returns pointer to element after the last (ie nullptr)
 AsteroidListItem* AsteroidList::end() {
-	return nullptr;
+    return nullptr;
 }
 
 const AsteroidListItem* AsteroidList::end() const {
-	return nullptr;
+    return nullptr;
 }
 
-// Create new list item with asteroid and add to list after prev
 AsteroidListItem* AsteroidList::insertAfter(AsteroidListItem* prev, Asteroid e) {
-	AsteroidListItem* newItem = new AsteroidListItem(e);
-	AsteroidListItem* oldNext = prev->setNext(newItem);
-	prev->getNext()->setNext(oldNext);
-	return newItem;
+    if (prev == nullptr)
+        return nullptr;
+    AsteroidListItem* newItem = new AsteroidListItem(e);
+    newItem->setNext(prev->setNext(newItem));
+    return newItem;
 }
 
-// Create new list items from source list and add to list after prev
 AsteroidListItem* AsteroidList::insertAfter(AsteroidListItem* prev, const AsteroidList& others) {
-	AsteroidListItem* iterator = prev;
-	AsteroidListItem* joinAtEnd = prev->getNext();
-	for (AsteroidListItem* i = (AsteroidListItem*) others.begin();
-			i != others.end(); i = i->getNext()
-	)
-		iterator = insertAfter(iterator, i->getData());
-	iterator->setNext(joinAtEnd);
-	return iterator;
+    for (AsteroidListItem* it = (AsteroidListItem*) others.begin();
+        it != others.end(); it = it->getNext()
+    )
+        prev = insertAfter(prev, it->getData());
+    return prev;
 }
 
-// Delete element from list after prev
 AsteroidListItem* AsteroidList::eraseAfter(AsteroidListItem* prev) {
-	if (!prev->hasNext())
-		return nullptr;
-	AsteroidListItem* toDelete = prev->setNext(prev->getNext()->getNext());
-	delete toDelete;
-	return prev->getNext();
+    if (!prev->hasNext())
+            return nullptr;
+    delete prev->setNext(prev->getNext()->getNext());
+    return prev->getNext();
 }
 
-// Removes all elements from list
 void AsteroidList::clear() {
-	while (head.hasNext()) {
-		AsteroidListItem* toDelete = head.setNext(head.getNext()->getNext());
-		delete toDelete;
-	}
+    while (!isEmpty())
+        eraseAfter(&head);
 }
 
-// Clears list and replaces it with copy of list from src
 AsteroidList& AsteroidList::operator=(const AsteroidList& src) {
-	clear();
-	insertAfter(&head, src);
-	return *this;
+    clear();
+    insertAfter(&head, src);
+    return *this;
 }
